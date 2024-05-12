7732362109c8fd668120f9a6878580f53395152c
shayan <shayan.rahmdel@gmail.com>
2024-05-12 17:02:09 +0330

    add
    add

---
+++ ApiGateway-sA-sB/src/test/java/com/example/apigatewaysasb/ApiGatewaySASBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewaysasb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySASBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ServiceB/src/main/java/com/example/serviceb/Controller.java
@@ -0,0 +1,16 @@
+package com.example.serviceb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+
+    @GetMapping("/micro2")
+    public String getService1(){
+        return "service2";
+    }
+}
